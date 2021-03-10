a504249e0dfed3cac44f01aa48d1dd81945fd8ee
HZGhoulz <78311772+HZGhoulz@users.noreply.github.com>
2021-03-10 11:47:28 -0500

    mute
    mute

--- index.js
+++ index.js
@@ -183,6 +183,19 @@ if (command === 'clean') {
 
     return;
 }
+if (command === 'mute') {
+    if(!message.member.hasPermission(['ADMINISTRATOR'])) return;
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+    if(member.hasPermission(['ADMINISTRATOR']) && !message.member.hasPermission('ADMINISTRATOR')) return;
+
+        let mutedRole = message.guild.roles.cache.get('819240702440767488');
+        let verifiedRole = message.guild.roles.cache.get('746765330520604732');
+        if(mutedRole) {
+            member.roles.add(mutedRole);
+            member.roles.remove(verifiedRole);
+            message.channel.send("User was Successfully muted.");
+        }
+}
 
 
 
