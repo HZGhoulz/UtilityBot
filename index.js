63f467772caf8be046e83a544ec0a970830800b0
HZGhoulz <78311772+HZGhoulz@users.noreply.github.com>
2021-03-18 09:14:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ if (command === 'say') {
     
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('You do not have permission to use that command.');
     let botmessage = args.join(" ");
-    if (!args) message.channel.send('Please list something for me to say. Example: -say Hello There')
+    if (!args[1]) message.channel.send('Please list something for me to say. Example: -say Hello There')
     message.delete().catch();
     message.channel.send(botmessage);
     return;
