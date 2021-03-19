421e81654180c1111399d244b9f2c7117120c171
HZGhoulz <78311772+HZGhoulz@users.noreply.github.com>
2021-03-19 08:23:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -182,7 +182,6 @@ if (command === 'clean') {
 if (command === 'mute') {
     if(!message.member.hasPermission(['ADMINISTRATOR'])) return;
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
-    if(member.hasPermission(['ADMINISTRATOR']) && !message.member.hasPermission('ADMINISTRATOR')) return;
     let modtomute = message.author
     let reason = args.join(" ").slice(22);
     if (!args[0]) return message.channel.send("Please list a member to mute. Example: -mute @user *reason*")
